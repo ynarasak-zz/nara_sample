8449799105e5580d54d1f51d1f5f8089e16f498c
narasaki <mduckfrogbby@gmail.com>
2018-10-30 14:10:11 +0900

    白くする

--- app/assets/javascripts/stylemarks.js
+++ app/assets/javascripts/stylemarks.js
@@ -1,16 +1,26 @@
+if ( document.body === void 0 || document.body === null ){
+}
+else if ( document.body != null ){
+  document.body.style = 'visibility:hidden'
+}
+
 window.onload = function() { 
-  var url_map = { 12345:'http://juna.officialblog.jp/archives/33522044.html', 1:'https://corp.stylemarks.jp/'}
-  var keys = location.pathname.match(/.*[/](.*?)$/);
-  var key = keys[keys.length-1]
-  if( url_map[key] === void 0){
-    //alert(keys+":"+key);
-    return
+  try {
+    var url_map = { 12345:'http://juna.officialblog.jp/archives/33522044.html', 1:'https://corp.stylemarks.jp/'}
+    var keys = location.pathname.match(/.*[/](.*?)$/);
+    var key = keys[keys.length-1]
+    if( url_map[key] === void 0){
+      document.body.style = 'visible'
+      throw new Error('end');
+    }
+    //alert('url :'+location.pathname+': key :'+key+': redirect :'+url_map[key]);
+    var redirect_url = url_map[key] + location.search;
+    if (document.referrer) {
+      var referrer = "referrer=" + encodeURIComponent(document.referrer);
+      redirect_url = redirect_url + (location.search ? '&' : '?') + referrer;
+    }
+    location.href = redirect_url;
+  }catch (e) {
+    
   }
-  //alert('url :'+location.pathname+': key :'+key+': redirect :'+url_map[key]);
-  var redirect_url = url_map[key] + location.search;
-  if (document.referrer) {
-    var referrer = "referrer=" + encodeURIComponent(document.referrer);
-    redirect_url = redirect_url + (location.search ? '&' : '?') + referrer;
-  }
-  location.href = redirect_url;
 }
